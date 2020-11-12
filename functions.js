62b4d7431af4d719d8a39b5cc1d5683ab0feb7c4
kovacs reka <reka.kovacs@nmg.ro>
2020-11-12 22:27:30 +0200

    improvement on hideing pages

--- functions.js
+++ functions.js
@@ -1,19 +1,34 @@
 function hide(id) {
     // document.getElementById('skills').style.display = 'none';
     var el = document.getElementById(id);
-    console.info("hide:" + id);
+    console.info(el);
     if (el) {
         el.style.display = "none";
     } else {
-        console.error("pagina nu exista" , id)
+        console.error("pagina nu exista" , id);
     }
 }
 
+// function hideAllPages() {
+//     hide ("home");
+//     hide ("skills");
+//     hide ("projects");
+//     hide ("languages");
+
+// } 
+
 function hideAllPages() {
-    hide ("home");
-    hide ("skills");
-    hide ("projects");
-    hide ("languages");
+    var pages = document.querySelectorAll(".page");
+    console.log(pages.length);
+
+    //for(initializare; conditie; post action-execute)
+    for(var i = 0; i <= pages.length-1; i++) {
+        var page = pages[i];
+        var id = page.id;
+        console.info("i=", i,"id=", id, page);
+        hide(id);
+    }
+    
 }
 
 function showHome() {
@@ -41,4 +56,4 @@ function showLanguages() {
 
 
 
-/* document.getElementById('projects').style.display = "none"; */
+// document.getElementById('projects').style.display = "none"; */
