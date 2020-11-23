3cd41f558eadf9339f3aee572d1a147ddf3ec763
kovacs reka <reka.kovacs@nmg.ro>
2020-11-23 19:26:09 +0200

    .....

--- functions.js
+++ functions.js
@@ -1,7 +1,20 @@
+var activePage = "skills";
+
+
+
+function hideAllPages() {
+    var pages = document.querySelectorAll(".page");
+    for(var i = 0; i <= pages.length-1; i++) {
+        var page = pages[i];
+        var id = page.id;
+        hide(id);
+    }
+}
+
+var activePage = "skills";
+
 function hide(id) {
-    // document.getElementById('skills').style.display = 'none';
     var el = document.getElementById(id);
-    console.info(el);
     if (el) {
         el.style.display = "none";
     } else {
@@ -9,50 +22,49 @@ function hide(id) {
     }
 }
 
-// function hideAllPages() {
-//     hide ("home");
-//     hide ("skills");
-//     hide ("projects");
-//     hide ("languages");
-
-// } 
-
-function hideAllPages() {
-    var pages = document.querySelectorAll(".page");
-    console.log(pages.length);
-
-    //for(initializare; conditie; post action-execute)
-    for(var i = 0; i <= pages.length-1; i++) {
-        var page = pages[i];
-        var id = page.id;
-        console.info("i=", i,"id=", id, page);
-        hide(id);
-    }
-    
+function hidePreviousPage () {
+    hide(activePage);
 }
 
 function showPage (pageId) {
-    hideAllPages();
+    hidePreviousPage();
     document.getElementById(pageId).style.display = "";
+    activePage = pageId;
 } 
 
-
-function showHome() {
-    showPage('home');
-} 
-
-function showSkills() {
-    showPage('skills'); 
+function initMenu() {
+    document.addEventListener("click", function(e){
+        var link = e.target;
+        if (link.matches("#top-menu-bar a")) {
+            var id = link.innerHTML.toLowerCase();
+            showPage(id);
+            link.className.add("active");
+        }
+    });
 }
 
-function showProjects() {
-    showPage('projects');
-}
+initMenu();
 
-function showLanguages() {
-    showPage('languages');
-}
+showPage(activePage);
+
+var skills = [
+    "HTML (15)",
+    "CSS (5)", 
+    "JS (20)"
+];
+
+var skillsLi_tmp = [
+    "<li>" + skills[0] + "</li>" +
+    "<li>" + skills[1] + "</li>" +
+    "<li>" + skills[2] + "</li>"
+];
+
+var skillsLi = skills.map(function(skill){
+    console.info(skill);
+    return "<li>" + skill + "</li>"; 
+});
 
 
-
-// document.getElementById('projects').style.display = "none"; */
+var ul = document.querySelector("#skills ul");
+ul.innerHTML =  skillsLi.join("");
+                
